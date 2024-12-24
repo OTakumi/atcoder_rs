24d901f7e00be555b9a48280334d79be6ed3a68d
OTakumi <takumi.o.0410@gmail.com>
2024-12-24 15:05:56 +0900

    ac

--- _result/abs/src/bin/abc086a.rs
+++ _result/abs/src/bin/abc086a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    let rslt = if a * b % 2 == 0 { "Even" } else { "Odd" };
+
+    println!("{}", rslt);
 }
