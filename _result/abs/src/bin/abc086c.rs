241555cc27ef575b636ecbd0d55f4971ac2ded30
OTakumi <takumi.o.0410@gmail.com>
2024-12-28 23:01:00 +0900

    ac abc086c

--- _result/abs/src/bin/abc086c.rs
+++ _result/abs/src/bin/abc086c.rs
@@ -1,3 +1,47 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut input: [(i64, i64, i64); n],
+    }
+
+    let mut prev = (0, 0, 0);
+
+    // Not feasible if coordinate distance is greater than time difference
+    // or if time difference is odd and coordinate distance is even
+    for (cur_t, cur_x, cur_y) in input {
+        let (pt, px, py) = prev;
+        let d = (cur_x - px).abs() + (cur_y - py).abs();
+
+        let dt = cur_t - pt;
+
+        if dt < d {
+            println!("No");
+            return;
+        }
+
+        if (dt - d) % 2 != 0 {
+            println!("No");
+            return;
+        }
+
+        prev = (cur_t, cur_x, cur_y);
+    }
+
+    println!("Yes");
 }
