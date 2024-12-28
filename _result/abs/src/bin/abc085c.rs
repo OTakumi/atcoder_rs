4545f860f1413ce2b3501e6f0a5ddf4117581701
OTakumi <takumi.o.0410@gmail.com>
2024-12-28 22:56:04 +0900

    ac abc085c

--- _result/abs/src/bin/abc085c.rs
+++ _result/abs/src/bin/abc085c.rs
@@ -1,3 +1,34 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
 fn main() {
-    todo!();
+    input! {
+        n: u64,
+        y: u64,
+    }
+
+    for ten in (0..=n).rev() {
+        for five in (0..=(n - ten)).rev() {
+            let one = n - ten - five;
+            if ten * 10000 + five * 5000 + one * 1000 == y {
+                println!("{} {} {}", ten, five, one);
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
