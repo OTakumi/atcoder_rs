336b834b36cfb60abaaf1d057543f399a58875b6
OTakumi <takumi.o.0410@gmail.com>
2024-12-28 23:00:35 +0900

    ac abc049c

--- _result/abs/src/bin/abc049c.rs
+++ _result/abs/src/bin/abc049c.rs
@@ -1,3 +1,32 @@
+#[allow(unused_imports)]
+use itertools::{iproduct, Itertools};
+#[allow(unused_imports)]
+use num_traits::pow;
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
 fn main() {
-    todo!();
+    input! {
+        mut s: String,
+    }
+
+    let s = s.replace("eraser", "");
+    let s = s.replace("erase", "");
+    let s = s.replace("dreamer", "");
+    let s = s.replace("dream", "");
+
+    if s.is_empty() {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
 }
